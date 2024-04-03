931bb29118dbd41b02a1fb4104d49375170185d0
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-04-03 12:21:44 +0530

    add files

---
+++ script.js
@@ -0,0 +1,10 @@
+const sinUp = document.querySelector("#sinUp")
+const logIn = document.querySelector("#logIn")
+ 
+sinUp.addEventListener('click' , () =>{
+     alert("update soon");
+});
+
+logIn.addEventListener("click" , () =>{
+     alert("uopdate soon");
+});
